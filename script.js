4642c6dbf88d6e6b7bf9c3208f43a7638e8faa45
Raveesh Samkaria <rishu9417@gmail.com>
2023-06-25 22:32:47 +0530

    Added: Javscript File

---
+++ script.js
@@ -0,0 +1,92 @@
+let btnRef = document.querySelectorAll(".button-option");
+let popUpRef = document.querySelector(".popup");
+let newGameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("msg");
+
+let winningPattern = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let xTurn = true;
+let count = 0;
+
+const disableButtons = () => {
+  btnRef.forEach((element) => (element.disabled = true));
+  popUpRef.classList.remove("hide");
+};
+
+const enableButtons = () => {
+  btnRef.forEach((element) => {
+    element.innerText = "";
+    element.disabled = false;
+  });
+  popUpRef.classList.add("hide");
+};
+
+newGameBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
+});
+
+restartBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
+});
+
+const winFunction = (letter) => {
+  disableButtons();
+  if (letter == "X") {
+    msgRef.innerHTML = "&#x1F389; <br> 'X' Wins";
+  } else {
+    msgRef.innerHTML = "&#x1F389; <br> 'O' Wins";
+  }
+};
+
+const drawFunction = () => {
+  disableButtons();
+  msgRef.innerHTML = "&#x1F60E; <br> It's a Draw";
+};
+
+const winChecker = () => {
+  for(let i of winningPattern) {
+    let [element1, element2, element3] = [
+      btnRef[i[0]].innerText,
+      btnRef[i[1]].innerText,
+      btnRef[i[2]].innerText,
+    ];
+    if (element1 != "" && element2 != "" && element3 != "") {
+      if (element1 == element2 && element2 == element3) {
+        winFunction(element1);
+      }
+    }
+  }
+};
+
+btnRef.forEach((element) => {
+    element.addEventListener("click", () => {
+    if (xTurn) {
+      xTurn = false;
+      element.innerText = "X";
+      element.disabled = true;
+    } else {
+      xTurn = true;
+      element.innerText = "O";
+      element.disabled = true;
+    }
+    count += 1;
+    if(count == 9) {
+      drawFunction();
+    }
+    winChecker();
+  });
+});
+
+window.onload = enableButtons;
